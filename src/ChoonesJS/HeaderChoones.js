1107ff22773c7fe52cd079d5f1d62c50a3671cc7
jerodthompie123 <jerod.thompson-springer@hotmail.com>
2022-08-10 20:53:41 -0400

    CSS

---
+++ src/ChoonesJS/HeaderChoones.js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+const HeaderChoones=()=>{
+    return(
+        <div className='head1'>
+            <h1 className='head2'>Choones</h1>
+        </div>
+
+    );
+}
---
+++ src/ChoonesJS/Navbar.js
@@ -0,0 +1,19 @@
+import React from 'react';
+
+const Navbar =()=>{
+    return(
+        
+        <div className='navbar'>
+            <ul>
+                <li>Moods</li>
+                <li>Saved Playlist</li>
+                <li>Now Playing</li>
+                <li>Settings</li>
+                <li>Log out</li>
+
+            </ul>
+            
+            
+        </div>
+    )
+}
