2fc5a3f61a1a949260817292721317db8686f1cf
yhan-z <yhan.g.a.robert@gmail.com>
2022-08-13 13:06:24 -0400

    git st

---
+++ src/pages/userPorfile.js
@@ -0,0 +1,29 @@
+import React from "react";
+
+function Profile() {
+<div>
+    <img/>
+    Recently you have been:
+    <div>
+        <ul>
+            <li>
+                Sad
+            </li>
+            <li>
+                Sad
+            </li>
+            <li>
+                Sad
+            </li>
+            <li>
+                Sad
+            </li>
+        </ul>
+    </div>
+
+    <div>
+        <p>your top choices</p>
+    </div>
+</div>
+}
+
