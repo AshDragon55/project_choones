10e5d25eb7d9155ba9d332baab6c06609ef3e736
Vashanti Raymond <31632539+vashanti@users.noreply.github.com>
2022-08-20 12:25:55 -0400

    Update App.js
    Removed duplications. Moved Route.

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, {useState, useEffect}from 'react';
 import './App.css';
 import Navbar from './components/Navbar';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
@@ -6,21 +6,14 @@ import Home from './Pages/Home';
 import Moods from './Pages/Moods';
 import Playing from './Pages/Playing';
 import HeaderChoones from './ChoonesJS/HeaderChoones';
-import Navbar from './ChoonesJS/Navbar';
 import Musicboard from './ChoonesJS/Musicboard';
-import Moods from './ChoonesJS/Moods';
-import Home from './ChoonesJS/Home';
 import axios from 'axios';
 
-import {BrowserRouter as Router,Route,Routes} from 'react-router-dom';
-
-import {useState, useEffect} from 'react';
 const CLIENT_ID="2389f6b1020f48a9bddf4050a103e4d7";
 const CLIENT_SECRET="44c15351760b4cdd890d7bc63c966dd8";
 
 
 function App() {
-
   return (
     <>
       <Router>
@@ -29,11 +22,11 @@ function App() {
           <Route path='/' exact element={<Home/>} />
           <Route path='/moods' element={<Moods/>} />
           <Route path='/playing' element={<Playing/>} />
+          <Route exact path='/musicboard' element={<Musicboard/>}></Route>
         </Routes>
       </Router>
     </>
    
-            <Route exact path='/musicboard' element={<Musicboard/>}></Route>
   );
 }
 
