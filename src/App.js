7836c769caefe8718a1baceb407cda0c5be7e2ea
jerodthompie123 <jerod.thompson-springer@hotmail.com>
2022-08-20 09:57:17 -0400

    new

--- src/App.js
+++ src/App.js
@@ -5,10 +5,17 @@ import Navbar from './ChoonesJS/Navbar';
 import Musicboard from './ChoonesJS/Musicboard';
 import Moods from './ChoonesJS/Moods';
 import Home from './ChoonesJS/Home';
+import axios from 'axios';
+
 import {BrowserRouter as Router,Route,Routes} from 'react-router-dom';
+
+import {useState, useEffect} from 'react';
+const CLIENT_ID="2389f6b1020f48a9bddf4050a103e4d7";
+const CLIENT_SECRET="44c15351760b4cdd890d7bc63c966dd8";
 
 
 function App() {
+
   return (
     <Router>
     <div className="App">
